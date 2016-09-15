97bc4cf28b896126983ab8d992411582200e5a03
Ward9250 <axolotlfan9250@gmail.com>
2016-09-15 23:22:28 +0100

    Code refactor

--- src/compute.jl
+++ src/compute.jl
@@ -35,47 +35,39 @@ function generate_names_lists(indexer::Indexer)
     return list1, list2
 end
 
-function pairwise_dates{D<:EvolutionaryDistance,S<:Sequence}(::Type{D}, sequences::Vector{S},
-    mu::Float64, method)
+function write_distances(filename::ASCIIString, names1::Vector{Symbol},
+    names2::Vector{Symbol}, dists::Vector{Float64})
 
-    slen = length(sequences[1])
-    for seq in sequences
-        @assert length(seq) == slen error("Sequences must be of the same length!")
+    distfile = open(filename, "w")
+    for i in 1:length(names1)
+        print(distfile, names1[i])
+        print(distfile, ", ")
+        print(distfile, names2[i])
+        print(distfile, ", ")
+        print(distfile, dists[i])
+        print(distfile, "\n")
     end
+    close(distfile)
+end
 
-    dists, vars = distance(D, sequences)
+function dates_from_dists(dists::Vector{Float64}, len::Int, mu::Float64, method)
     Ts = zeros(Float64, length(dists))
-
-    @inbounds for i in 1:length(dist)
-        nmut = convert(Int, ceil(dists[i] * slen))
+    @inbounds for i in 1:length(dists)
+        nmut = convert(Int, ceil(dists[i] * len))
         Ts[i] = Dating.middle(coaltime(slen, nmut, mu, method))
     end
     return Ts
 end
 
-#=
-function pairwise_dates{D<:EvolutionaryDistance,S<:Sequence}(sequences::Vector{S},
-    mu::Float64, model::Type{D}, method, width::Int64, length::Int64)
-
-
-
-    slen = length(sequences)
-    times = zeros(Float64, slen, slen)
-    for i = 1:slen, j = i+1:slen
-        Dist, Var = distance(model, sequences[i], sequences[j])
-        nmut = convert(Int, ceil(Dist * slen))
-        T = coaltime(length(sequences[i]), nmut, mu, method)
-        mT = Dating.middle(T)
-        times[i, j] = times[j, i] = mT
-    end
-    return times
-end
-=#
-
 function compute(args)
 
     index, sequences = gather_sequences(args)
     names1, names2 = generate_names_lists(index)
+
+    slen = length(sequences[1])
+    for seq in sequences
+        @assert length(seq) == slen error("Sequences must be of the same length!")
+    end
 
     m = lowercase(args["model"])
     if m == "jc69"
@@ -85,25 +77,16 @@ function compute(args)
     else
         error("Invalid choice of distance model.")
     end
-    distfile = open("$(args["outfile"])_distances.txt", "w")
     if args["scan"]
         if args["step"] <= 0
             args["step"] = args["width"]
         end
-        dists = distance(model, sequences, args["width"], args["step"])
+        dists, vars = distance(model, sequences, args["width"], args["step"])
     else
         dists, vars = distance(model, sequences)
-        for i in 1:length(names1)
-            print(distfile, names1[i])
-            print(distfile, ", ")
-            print(distfile, names2[i])
-            print(distfile, ", ")
-            print(distfile, dists[i])
-            print(distfile, "\n")
-        end
     end
-    close(distfile)
-#=
+    write_distances("$(args["outfile"])_distances.txt", names1, names2, dists)
+
     met = lowercase(args["method"])
     if met == "default"
         dmethod = SpeedDating
@@ -112,16 +95,6 @@ function compute(args)
     else
         error("Invalid choice of coalescence time estimate method.")
     end
-
-
-
-
-
-        times = pairwise_dates(sequences, args["mutation_rate"], model, dmethod, )
-
-        times = pairwise_dates(sequences, args["mutation_rate"], model, dmethod)
-        writedlm("$(args["outfile"]).txt", times)
-=#
-
+    times = dates_from_dists(dists, slen, args["mutation_rate"], dmethod)
 
 end
