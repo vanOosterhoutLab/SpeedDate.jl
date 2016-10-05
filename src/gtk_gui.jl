25ba4d1e51eddad19d10b3d55489f1618406cbd2
Ward9250 <ward9250@gmail.com>
2016-10-05 21:49:25 +0100

    Added sliding window box

--- src/gtk_gui.jl
+++ src/gtk_gui.jl
@@ -17,7 +17,7 @@ function start_interactive_app()
     push!(actions_frame, button_box)
     push!(button_box, load_button)
     push!(button_box, go_button)
-    g[1,1] = actions_frame
+    g[1, 1] = actions_frame
 
     ## Calculation and model assumptions
     μ_box = @Box(:h)
@@ -34,13 +34,29 @@ function start_interactive_app()
     push!(model_box, JC69_radio)
     push!(model_box, K80_radio)
     push!(model_box, μ_box)
-    g[2,1] = model_frame
+    g[2, 1] = model_frame
 
 
     ## Sliding window settings
     window_frame = @Frame("Sliding Window")
     window_box = @Box(:v)
     slide_check = @CheckButton("Use Sliding Windows")
+    window_box_2 = @Box(:h)
+    size_label = @Label("Window Size")
+    size_entry = @Entry()
+    window_box_3 = @Box(:h)
+    step_label = @Label("Step Size")
+    step_entry = @Entry()
+    push!(window_box_2, size_label)
+    push!(window_box_2, size_entry)
+    push!(window_box_3, step_label)
+    push!(window_box_3, step_entry)
+    push!(window_box, slide_check)
+    push!(window_box, window_box_2)
+    push!(window_box, window_box_3)
+    push!(window_frame, window_box)
+    g[3, 1] = window_frame
+
 
     ## Signals and behaviour
 
@@ -66,8 +82,6 @@ function start_interactive_app()
 
     push!(win, g)
     setproperty!(g, :column_spacing, 15)
-    setproperty!(g, :row_spacing, 15)
-    setproperty!(g, :column_homogeneous, true)
     showall(win)
     c = Condition()
     signal_connect(win, :destroy) do widget
